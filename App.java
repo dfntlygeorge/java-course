445c4edf83b025448df5ae3e548e9a7bc55d6162
Konrads <g.a.donayre.business@gmail.com>
2025-01-28 12:50:35 +0800

    Exercise: MAD LIBS GAME

--- App.java
+++ App.java
@@ -1,27 +1,31 @@
 import java.util.Scanner;
 
 public class App {
-
     public static void main(String[] args) {
-        // Calculate area of a rectangle
-        double length = 0;
-        double width = 0;
-        double area = 0;
-        String unit = "";
+        // MAD LIBS GAME
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter a unit: ");
-        unit = sc.nextLine();
+        String adjective1;
+        String adjective2;
+        String adjective3;
+        String noun1;
+        String verb1;
 
-        System.out.print("Enter length: ");
-        length = sc.nextDouble();
+        System.out.print("Enter an adjective (description): ");
+        adjective1 = sc.nextLine();
+        System.out.print("Enter a verb: ");
+        verb1 = sc.nextLine();
+        System.out.print("Enter an adjective (description): ");
+        adjective2 = sc.nextLine();
+        System.out.print("Enter a noun: ");
+        noun1 = sc.nextLine();
+        System.out.print("Enter an adjective (description): ");
+        adjective3 = sc.nextLine();
 
-        System.out.print("Enter width: ");
-        width = sc.nextDouble();
-
-        area = length * width;
-
-        System.out.println("Area: " + area + " " + unit + "²");
+        System.out.println("\nToday I went to a " + adjective1 + " zoo.");
+        System.out.println("In an exhibit, I saw a " + noun1 + ".");
+        System.out.println(noun1 + " was " + adjective2 + " and " + verb1 + "!");
+        System.out.println("I was " + adjective3 + "!");
 
         sc.close();
 

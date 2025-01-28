3d67172efbd914a7cc17ffdab6e701bd125c2bb9
Konrads <g.a.donayre.business@gmail.com>
2025-01-28 11:45:32 +0800

    java intro

---
+++ App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("Fuck it");
+        System.out.println("Hatdog");
+        // sout for snippet
+        System.out.println("Hello World!");
+    }
+}
